33c6bffc64c0d48239eb9ea67ec64ec02848602c
sm8sk4l <sebastianmoskal@protonmail.com>
2022-10-11 20:21:13 +0200

    lab-2

--- react-wsei/src/App.tsx
+++ react-wsei/src/App.tsx
@@ -1,5 +1,22 @@
-function App() {
-  return <div className="App"></div>;
-}
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Home } from "./components/routes/home/home";
+import { FC } from "react";
+import { Login } from "./components/routes/login/login";
+
+export const App: FC = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Link to="login">Login</Link>
+        <Link to="/">Home</Link>
+      </div>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
---
+++ react-wsei/src/components/routes/login/login.tsx
@@ -0,0 +1,5 @@
+import { FC } from "react";
+
+export const Login: FC = () => {
+  return <h2>Login Page</h2>;
+};
